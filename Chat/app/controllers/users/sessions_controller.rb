93475444db6f8f7c24bc822800d3911a1a911bae
arthurBK <arthurbonnecarrere@hotmail.fr>
2016-12-25 12:29:48 +0100

    formatting

--- Chat/app/controllers/users/sessions_controller.rb
+++ Chat/app/controllers/users/sessions_controller.rb
@@ -23,11 +23,11 @@ class Users::SessionsController < Devise::SessionsController
 
   protected
 
-  # def after_sign_up_path_for(resource)
-    # super(resource)
+  def after_sign_up_path_for(resource)
+    super(resource)
   # byebug
-    # new_post_path
-  # end
+    new_chatroom_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
