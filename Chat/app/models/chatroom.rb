d6330e50571e64c7187b7e5a0bfe1d79cada23d8
Arthur BONNECARRERE <abonneca@e2r7p7.42.fr>
2016-12-27 17:49:50 +0100

    "ok"

--- Chat/app/models/chatroom.rb
+++ Chat/app/models/chatroom.rb
@@ -1,4 +1,8 @@
 class Chatroom < ApplicationRecord
   belongs_to :user
   has_many :posts
+  validates :name, length: {
+    maximum: 1,
+    too_long: "Please choose a name that is only 1 word."
+  }
 end
